a8d9f815d07f307a1c8729094499376e41530eaa
Kenny <kennykline12@pm.me>
2026-04-30 11:33:24 -0700

    styling for the header

--- app/components/ui/header/header.tsx
+++ app/components/ui/header/header.tsx
@@ -1,20 +1,39 @@
+import RefractLogoComponent from '~/src/RefractLogo';
 import { AvatarDropdown } from './avatar-dropdown';
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
+
+const baseTabClassName = 'text-brownish border border-gray-200 px-12 py-3 rounded-md'
+const activeTabClassName = 'font-semibold'
+const inactiveTabClassName = 'bg-gray-100 hover:bg-lightgray'
 
 export function Header() {
+  const location = useLocation();
+  const isDashboardActive = location.pathname.match(/^\/items\/\d+$/) || location.pathname === '/';
+  const isDatabaseViewActive = location.pathname === '/items/all';
+
   return (
     <header className="border-b bg-background">
-      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-2">
+      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-8">
         {/* Left side */}
         <div className="flex items-center gap-6">
-          <span className="text-lg font-semibold">Refract</span>
+          <div className="flex flex-col items-center">
+            <div className="flex items-center gap-2">
+              <RefractLogoComponent />
+              <p className="text-2xl font-">Refract</p>
+            </div>
+            <p className='text-xs'>by Lightcast</p>
+          </div>
 
           <nav className="hidden md:flex gap-4 text-sm text-muted-foreground">
-            <Link to="/" className="hover:text-foreground">
-              Dashboard
+            <Link to="/">
+              <p className={`${baseTabClassName} ${isDashboardActive ? activeTabClassName : inactiveTabClassName}`}>
+                Dashboard
+              </p>
             </Link>
-            <Link to="/items/all" className="hover:text-foreground">
-              Database View
+            <Link to="/items/all">
+              <p className={`${baseTabClassName} ${isDatabaseViewActive ? activeTabClassName : inactiveTabClassName}`}>
+                Database View
+              </p>
             </Link>
           </nav>
         </div>
